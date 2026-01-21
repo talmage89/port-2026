6b177d18616acdd86e27803142cfb6faf9b8bcc1
tibble <info@talmage.dev>
2026-01-20 22:50:11 -0700

    feat: some styling tweaks

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -3,8 +3,9 @@ import type { Article as ArticleType } from "@/prisma/generated/client";
 export function Article({ article }: { article: ArticleType }) {
   return (
     <section className="space-y-8">
-      <h1 className="font-semibold text-3xl leading-tight tracking-tight">{article.title}</h1>
-      {article.link && (
+      <h1 className="font-bold text-3xl leading-tight tracking-tight">{article.title}</h1>
+      <span className="flex items-center gap-1 text-sm">
+        <p>Source:</p>
         <a
           href={article.link}
           target="_blank"
@@ -13,7 +14,7 @@ export function Article({ article }: { article: ArticleType }) {
         >
           {article.link}
         </a>
-      )}
+      </span>
       <div className="text-black/70 text-lg leading-relaxed">
         <p className="whitespace-pre-wrap">{article.summary}</p>
       </div>
