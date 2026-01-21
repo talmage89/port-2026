e2d5f941d9364d1eb2fdfa0e6f42ab0d1df91ce8
tibble <info@talmage.dev>
2026-01-20 19:57:55 -0700

    chore: re-enable db url

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   PORT: z.preprocess((val) => parseInt(String(val), 10), z.number()),
-  //   DATABASE_URL: z.string(),
+  DB_URL: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
