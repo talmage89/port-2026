71b3f3911ff71e35b86ded4f6c9db82621fbdbce
tibble <info@talmage.dev>
2026-01-28 21:15:50 -0700

    feat: dark mode

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -10,12 +10,12 @@ export function Article({ article }: { article: ArticleType }) {
           href={article.link}
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-block text-blue-600 underline hover:text-blue-800"
+          className="inline-block text-blue-400 underline hover:text-blue-300"
         >
           {article.link}
         </a>
       </span>
-      <div className="text-black/70 text-lg leading-relaxed">
+      <div className="text-white/70 text-lg leading-relaxed">
         <p className="whitespace-pre-wrap">{article.summary}</p>
       </div>
     </section>
